4d10988b0620af1617979b8707e41e5331fcf5f0
John Lawlor <jmlawlor@gmail.com>
2022-10-19 15:22:24 -0400

    Update imr_custom.js

--- imr_custom.js
+++ imr_custom.js
@@ -1,8 +1,12 @@
-﻿function CheckRegion() {
+function CheckRegion() {
     if (document.getElementById('capture_traditionalRegistration_dataRegion').value == "NA")
         document.getElementById('capture_traditionalRegistration_form_item_dataWWID').style.display = "none";
     else
         document.getElementById('capture_traditionalRegistration_form_item_dataWWID').style.display = "block";
 }
-CheckRegion();
-document.getElementById('capture_traditionalRegistration_dataRegion').onchange = CheckRegion();
+function initCheckRegion() {
+    document.getElementById('capture_traditionalRegistration_dataRegion').onchange = CheckRegion();
+    CheckRegion();
+}
+
+janrainCaptureWidgetOnLoad = initCheckRegion;
