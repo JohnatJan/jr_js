d7c029c653021795d01f1579e055c033c53af777
John Lawlor <jmlawlor@gmail.com>
2022-10-19 14:59:22 -0400

    initial draft of imr custom logic
    includes show/hide logic for WWID based on Region

---
+++ imr_custom.js
@@ -0,0 +1,8 @@
+﻿function CheckRegion() {
+    if (document.getElementById('capture_traditionalRegistration_dataRegion').value == "NA")
+        document.getElementById('capture_traditionalRegistration_form_item_dataWWID').style.display = "none";
+    else
+        document.getElementById('capture_traditionalRegistration_form_item_dataWWID').style.display = "block";
+}
+CheckRegion();
+document.getElementById('capture_traditionalRegistration_dataRegion').onchange = CheckRegion();
