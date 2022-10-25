a525d7eb046481ad1d031533dcbdbc12ae72e6b9
John Lawlor <jmlawlor@gmail.com>
2022-10-25 16:45:09 -0400

    Update imr_custom.js

--- imr_custom.js
+++ imr_custom.js
@@ -9,6 +9,6 @@ function initCheckRegion() {
     CheckRegion();
 }
 function janrainCaptureWidgetOnLoad(){
-  janrain.events.onCaptureScreenShow.addHandler(initCheckRegion);
+  janrain.events.onCaptureRenderComplete.addHandler(initCheckRegion);
   janrain.capture.ui.start();
 }
