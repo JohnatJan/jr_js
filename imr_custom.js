899ba47d2b139d73e7b41c36a29c3c6d90d68201
John Lawlor <jmlawlor@gmail.com>
2022-11-01 16:29:01 -0400

    Update imr_custom.js

--- imr_custom.js
+++ imr_custom.js
@@ -13,34 +13,11 @@ function initCheckRegion() {
     }catch (err) {}
 }
 
-// This function is called by the Capture Widget when it has completred loading
-// itself and all other dependencies. This function is required, and must call
-// janrain.capture.ui.start() for the Widget to initialize correctly.
 function janrainCaptureWidgetOnLoad() {
-
-    /*--
-        SHOW EVENTS:
-        This function will log Janrain events in your browser's console. You must
-        include janrain-utils.js to run this function. Comment this line to hide 
-        event logging.
-                                                                            --*/
-    janrainUtilityFunctions().showEvents();
-
-    /*--
-        SHOW FLOW VERSION:
-        This event handler shows the flow version in the specified element.
-        This is primarily for our developers' convenience, but your developers
-        may also find it useful.
-                                                                            --*/
-
+                                                                            
     janrain.events.onCaptureRenderStart.addHandler(function(result) {
           initCheckRegion()
     });
 
-    /*                                                                        *\
-    || *** CUSTOM ONLOAD CODE END ***                                         ||
-    \*========================================================================*/
-
-    // This should be the last line in janrainCaptureWidgetOnLoad()
     janrain.capture.ui.start();
 }
