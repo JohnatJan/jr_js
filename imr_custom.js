c72dcdf19dad90bea4f47608a69fafc87dc924de
John Lawlor <jmlawlor@gmail.com>
2022-11-01 15:33:39 -0400

    Update imr_custom.js

--- imr_custom.js
+++ imr_custom.js
@@ -5,11 +5,15 @@ function CheckRegion() {
         document.getElementById('capture_traditionalRegistration_form_item_dataWWID').style.display = "block";
 }
 function initCheckRegion() {
-    while (document.getElementById('capture_traditionalRegistration_dataRegion') == null) {
+        while (document.getElementById('capture_traditionalRegistration_dataRegion') == null) {
         try {
-        document.getElementById('capture_traditionalRegistration_dataRegion').onchange = CheckRegion;
+            document.getElementById('capture_traditionalRegistration_dataRegion').onchange = CheckRegion;
+            break;
         }catch (err) {}
         setTimeout(500);
     }
     CheckRegion();
 }
+janrain.events.onCaptureScreenShow.addHandler(function(result){
+     initCheckRegion;
+    });
