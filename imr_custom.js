4634643233931df711846a79b43020f79a9ee652
John Lawlor <jmlawlor@gmail.com>
2023-02-16 11:49:07 -0500

    Cleaned out old logic

--- imr_custom.js
+++ imr_custom.js
@@ -1,22 +1,3 @@
-function CheckRegion() {
-    if (document.getElementById('capture_traditionalRegistration_dataRegion').value == "NA")
-        document.getElementById('capture_traditionalRegistration_form_item_dataWWID').style.display = "none";
-    else
-        document.getElementById('capture_traditionalRegistration_form_item_dataWWID').style.display = "block";
-}
-function initCheckRegion() {
-    console.log("attempting to wire event.");
-    try {
-        //document.getElementById('capture_traditionalRegistration_dataRegion').onchange = CheckRegion;
-        document.getElementById('capture_traditionalRegistration_dataRegion').addEventListener('change', CheckRegion);
-        console.log("after document.getElementById('capture_traditionalRegistration_dataRegion').addEventListener('change', CheckRegion);");
-
-        CheckRegion();
-        return true;
-    }catch (err) {
-        console.log(err);
-    }
-}
 (function() {
     if (typeof window.janrain !== 'object') window.janrain = {};
     if (typeof window.janrain.settings !== 'object') window.janrain.settings = {};
