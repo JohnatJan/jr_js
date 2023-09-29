2f957aaacf46ecd42570ca1bcc3783441be31e83
John Lawlor <jmlawlor@gmail.com>
2023-09-29 10:16:11 -0400

    Update imr_custom.js

--- imr_custom.js
+++ imr_custom.js
@@ -6,12 +6,19 @@
         janrain.ready = true; 
         janrain.events.onCaptureRenderComplete.addHandler(function(result) {
                 console.log(janrain.gizmo.screenToRender);
+                //Psuedo-Activation Path
                 if(janrain.gizmo.screenToRender == 'verifyEmailSuccess') 
                 {
+                    janrain.capture.ui.renderScreen("changePasswordNoAuthForm");
                     //Show set password controls
                     const urlParams = new URLSearchParams(window.location.search);
                     const emailAddress = urlParams.get('login_hint');
                     document.getElementsByName("signInEmailAddress")[0].value = emailAddress; 
+                }
+                //Kill session after no-auth reset password.
+                if(janrain.gizmo.screenToRender == "changePasswordSuccess") {
+                    janrain.capture.ui.endCaptureSession();
+                    janrain.capture.ui.renderScreen("signIn");
                 }
         });
 
